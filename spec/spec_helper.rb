3bb15ef3af9dadfd06900901931c43076f3b71e5
Andre Smith <andrepsmithjr@gmail.com>
2014-01-14 10:28:08 -0800

    comment out webkit references 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,6 @@ class FeatureSpec < MiniTest::Spec
   #require "capybara/webkit"
   include Capybara::DSL
   Capybara.default_driver = :poltergeist
-  Capybara.javascript_driver = :poltergeist
+  #Capybara.javascript_driver = :poltergeist
   register_spec_type(/page$/, self)
 end
